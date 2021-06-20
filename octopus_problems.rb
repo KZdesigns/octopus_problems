c0143e8571ccf0e3baa75ea455ab2ecaed067b74
Kyle Zimmerman <kzdesignsios@gmail.com>
2021-06-19 21:02:10 -0700

    clever_octopus O(n) complete and working

--- octopus_problems.rb
+++ octopus_problems.rb
@@ -1,24 +1,38 @@
 #Sluggish Octopus O(n^2)
 def sluggish_octopus(fish)
 
-    fish.each_with_index do |fish_1, idx_1|
-        longest_fish = true
 
-        fish.each_with_index do |fish_2, idx_2|
-            next if idx_1 == idx_2
-
-            longest_fish = false if fish_2.length > fish_1.length
-        end
-
-        return fish_1 if longest_fish
-    end
 end
+
+# Dominant Octopus O(n log n) (merge_sort)
+
+
+
+# Clever Octopus O(n) (1 iteration)
+def clever_octopus(fish)
+    biggest_fish = fish.first
+
+    fish.each do |fish|
+        if biggest_fish.length < fish.length
+            biggest_fish = fish
+        end
+    end
+
+    biggest_fish
+end
+
+
 
 
 # => "fiiiissshhhhhh"
 fish = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
 
-puts sluggish_octopus(fish)
+# puts sluggish_octopus(fish)
+
+# puts clever_octopus(fish)
+
+# puts dominant_octopus(fish)
+
 
 
 
